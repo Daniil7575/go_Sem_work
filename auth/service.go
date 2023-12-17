8051786b4312b6dc52d4b2e1d42c64c8d0c4aede
Daniil7575 <spider2013vip@mail.ru>
2023-12-17 21:07:53 +0300

    delete

--- auth/service.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "context"
-
-type Service interface {
-	SignIn(ctx context.Context, username string, password string) (string, error)
-	SignUp(ctx context.Context, username string, password string) (string, error)
-}
